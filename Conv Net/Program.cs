cb85c771b01a99900d7dd6c15e2ee72b31afa95c
abc 123 <darkmatter_9@hotmail.com>
2021-03-03 17:48:51 -0500

    aa

--- Conv Net/Program.cs
+++ Conv Net/Program.cs
@@ -20,8 +20,8 @@ namespace Conv_Net {
         public static MathNet.Numerics.Distributions.Normal normalDist = new MathNet.Numerics.Distributions.Normal(0, 1, rand);
         public static Stopwatch stopwatch = new Stopwatch();
 
-        // public static Net NN = new Net();
-        public static Conv_Net CNN = new Conv_Net();
+        public static Net NN = new Net();
+        // public static Conv_Net CNN = new Conv_Net();
         public static Double eta = 0.01;
         public static int batchSize = 16;
 
@@ -43,43 +43,103 @@ namespace Conv_Net {
 
 
 
-            test_CNN(testing_sample_size);
-            for (int epoch = 0; epoch < 10; epoch++) {
-                Console.WriteLine("______________________________________________________________________\nEPOCH: " + epoch);
-
-                Utils.shuffle_Tensor(training_images, training_labels);
-                train_CNN(CNN_training_sample_size, batchSize);
-                test_CNN(testing_sample_size);
-            }
-
-            //test_NN(testing_sample_size);
+            //test_CNN(testing_sample_size);
             //for (int epoch = 0; epoch < 10; epoch++) {
-            //    Console.WriteLine("++++++++++++++++++++++++++++++++");
-            //    Console.WriteLine("Epoch: " + epoch);
-
+            //    Console.WriteLine("____________________________________________________________\nEPOCH: " + epoch);
             //    Utils.shuffle_Tensor(training_images, training_labels);
-
-            //    stopwatch.Start();
-            //    train_NN(training_sample_size, batchSize);
-            //    test_NN(testing_sample_size);
-            //    stopwatch.Stop();
-            //    Console.WriteLine("Time elapsed: " + stopwatch.Elapsed);
-            //    stopwatch.Reset();
+            //    train_CNN(CNN_training_sample_size, batchSize);
+            //    test_CNN(testing_sample_size);
             //}
+
+            test_NN(testing_sample_size);
+            for (int epoch = 0; epoch < 10; epoch++) {
+                Console.WriteLine("____________________________________________________________\nEPOCH: " + epoch);
+                Utils.shuffle_Tensor(training_images, training_labels);
+                train_NN(training_sample_size, batchSize);
+                test_NN(testing_sample_size);
+            }
         }
 
 
-        static void test_CNN(int testing_sample_size) {
-            stopwatch.Start();
+        //static void test_CNN(int testing_sample_size) {
+        //    stopwatch.Start();
            
+        //    int correct = 0;
+        //    Double total_cross_entropy_loss = 0.0;
+        //    Tuple<Tensor, Tensor> t;
+
+        //    t = CNN.forward(testing_images, testing_labels);
+
+        //    for (int i = 0; i < testing_sample_size; i++) {
+        //        total_cross_entropy_loss += t.Item1.values[i];
+
+        //        int index_max_value_output = -1;
+        //        Double max_output = Double.MinValue;
+
+        //        int index_max_value_label = -1;
+        //        Double max_label = Double.MinValue;
+
+        //        for (int j = 0; j < t.Item2.dim_2; j++) {
+        //            if (t.Item2.values[i * t.Item2.dim_2 + j] > max_output) {
+        //                max_output = t.Item2.values[i * t.Item2.dim_2 + j];
+        //                index_max_value_output = j;
+        //            }
+        //            if (testing_labels.values[i * t.Item2.dim_2 + j] > index_max_value_label) {
+        //                max_label = testing_labels.values[i * t.Item2.dim_2 + j];
+        //                index_max_value_label = j;
+        //            }
+        //        }
+        //        if (index_max_value_output == index_max_value_label) {
+        //            correct++;
+        //        }
+        //    }
+        //    stopwatch.Stop();
+        //    Console.WriteLine("Testing time:\t" + stopwatch.Elapsed);
+        //    Console.WriteLine("Accuracy:\t" + (Double)correct / testing_sample_size * 100 + "% (" + correct + " correct out of " + testing_sample_size + ")");
+        //    Console.WriteLine("Average loss:\t" + total_cross_entropy_loss / testing_sample_size);
+        //    stopwatch.Reset();
+        //}
+
+        //static void train_CNN(int training_sample_size, int batch_size) {
+            
+        //    stopwatch.Start();
+
+        //    int num_batches = training_sample_size / batch_size;
+        //    int remainder = training_sample_size - num_batches * batch_size;
+        //    Tensor A;
+        //    Tensor B;
+        //    Tuple<Tensor, Tensor> R;
+
+        //    for (int i = 0; i < num_batches; i++) {
+        //        A = training_images.subset(i * batch_size, batch_size);
+        //        B = training_labels.subset(i * batch_size, batch_size);
+        //        R = CNN.forward(A, B);
+        //        CNN.backward();
+        //        CNN.update(batch_size);
+        //    }
+        //    if (remainder != 0) {
+        //        A = training_images.subset(num_batches * batch_size, remainder);
+        //        B = training_labels.subset(num_batches * batch_size, remainder);
+        //        R = CNN.forward(A, B);
+        //        CNN.backward();
+        //        CNN.update(remainder);
+        //    }
+        //    stopwatch.Stop();
+        //    Console.WriteLine("Training time:\t" + stopwatch.Elapsed + "\n");
+        //    stopwatch.Reset();
+        //}
+
+        static void test_NN(int testing_sample_size) {
+            stopwatch.Start();
+
             int correct = 0;
             Double total_cross_entropy_loss = 0.0;
             Tuple<Tensor, Tensor> t;
 
-            t = CNN.forward(testing_images, testing_labels);
+            t = NN.forward(testing_images, testing_labels);
 
             for (int i = 0; i < testing_sample_size; i++) {
-                total_cross_entropy_loss += t.Item1.values[i];
+                total_cross_entropy_loss += t.Item2.values[i];
 
                 int index_max_value_output = -1;
                 Double max_output = Double.MinValue;
@@ -87,13 +147,13 @@ namespace Conv_Net {
                 int index_max_value_label = -1;
                 Double max_label = Double.MinValue;
 
-                for (int j = 0; j < t.Item2.dim_2; j++) {
-                    if (t.Item2.values[i * t.Item2.dim_2 + j] > max_output) {
-                        max_output = t.Item2.values[i * t.Item2.dim_2 + j];
+                for (int j = 0; j < t.Item1.dim_2; j++) {
+                    if (t.Item1.values[i * t.Item1.dim_2 + j] > max_output) {
+                        max_output = t.Item1.values[i * t.Item1.dim_2 + j];
                         index_max_value_output = j;
                     }
-                    if (testing_labels.values[i * t.Item2.dim_2 + j] > index_max_value_label) {
-                        max_label = testing_labels.values[i * t.Item2.dim_2 + j];
+                    if (testing_labels.values[i * t.Item1.dim_2 + j] > max_label) {
+                        max_label = testing_labels.values[i * t.Item1.dim_2 + j];
                         index_max_value_label = j;
                     }
                 }
@@ -102,14 +162,14 @@ namespace Conv_Net {
                 }
             }
             stopwatch.Stop();
-            Console.WriteLine("Time elapsed for testing:\t" + stopwatch.Elapsed);
-            Console.WriteLine("Accuracy:\t\t\t" + (Double)correct / testing_sample_size * 100 + "% (" + correct + " correct out of " + testing_sample_size + ")");
-            Console.WriteLine("Average cross entropy loss:\t" + total_cross_entropy_loss / testing_sample_size);
+            Console.WriteLine("Testing time:\t" + stopwatch.Elapsed);
+            Console.WriteLine("Accuracy:\t" + (Double)correct / testing_sample_size * 100 + "% (" + correct + " correct out of " + testing_sample_size + ")");
+            Console.WriteLine("Average loss:\t" + total_cross_entropy_loss / testing_sample_size);
             stopwatch.Reset();
         }
 
-        static void train_CNN(int training_sample_size, int batch_size) {
-            
+        static void train_NN(int training_sample_size, int batch_size) {
+
             stopwatch.Start();
 
             int num_batches = training_sample_size / batch_size;
@@ -121,78 +181,21 @@ namespace Conv_Net {
             for (int i = 0; i < num_batches; i++) {
                 A = training_images.subset(i * batch_size, batch_size);
                 B = training_labels.subset(i * batch_size, batch_size);
-                R = CNN.forward(A, B);
-                CNN.backward();
-                CNN.update(batch_size);
+                R = NN.forward(A, B);
+                NN.backward();
+                NN.update(batch_size);
             }
             if (remainder != 0) {
                 A = training_images.subset(num_batches * batch_size, remainder);
                 B = training_labels.subset(num_batches * batch_size, remainder);
-                R = CNN.forward(A, B);
-                CNN.backward();
-                CNN.update(remainder);
+                R = NN.forward(A, B);
+                NN.backward();
+                NN.update(remainder);
             }
+
             stopwatch.Stop();
-            Console.WriteLine("Time elapsed for training:\t" + stopwatch.Elapsed + "\n");
+            Console.WriteLine("Training time:\t" + stopwatch.Elapsed + "\n");
             stopwatch.Reset();
         }
-
-        //static void test_NN(int testing_sample_size) {
-
-        //    int correct = 0;
-        //    Double total_cross_entropy_loss = 0.0;
-        //    Tuple<Tensor, Tensor> t;
-
-        //    t = NN.forward(testing_images, testing_labels);
-
-        //    for (int i = 0; i < testing_sample_size; i++) {
-        //        total_cross_entropy_loss += t.Item2.values[i];
-
-        //        int index_max_value_output = -1;
-        //        Double max_output = Double.MinValue;
-
-        //        int index_max_value_label = -1;
-        //        Double max_label = Double.MinValue;
-
-        //        for (int j = 0; j < t.Item1.dim_2; j++) {
-        //            if (t.Item1.values[i * t.Item1.dim_2 + j] > max_output) {
-        //                max_output = t.Item1.values[i * t.Item1.dim_2 + j];
-        //                index_max_value_output = j;
-        //            }
-        //            if (testing_labels.values[i * t.Item1.dim_2 + j] > max_label) {
-        //                max_label = testing_labels.values[i * t.Item1.dim_2 + j];
-        //                index_max_value_label = j;
-        //            }
-        //        }
-        //        if (index_max_value_output == index_max_value_label) {
-        //            correct++;
-        //        }
-        //    }
-        //    Console.WriteLine(correct + " correct out of " + testing_sample_size + ". \t Accuracy " + (Double)correct / testing_sample_size * 100 + "%");
-        //    Console.WriteLine("Average cross entropy loss: " + total_cross_entropy_loss / testing_sample_size);
-        //}
-
-        //static void train_NN(int training_sample_size, int batch_size) {
-        //    int num_batches = training_sample_size / batch_size;
-        //    int remainder = training_sample_size - num_batches * batch_size;
-        //    Tensor A;
-        //    Tensor B;
-        //    Tuple<Tensor, Tensor> R;
-
-        //    for (int i = 0; i < num_batches; i++) {
-        //        A = training_images.subset(i * batch_size, batch_size);
-        //        B = training_labels.subset(i * batch_size, batch_size);
-        //        R = NN.forward(A, B);
-        //        NN.backward();
-        //        NN.update(batch_size);
-        //    }
-        //    if (remainder != 0) {
-        //        A = training_images.subset(num_batches * batch_size, remainder);
-        //        B = training_labels.subset(num_batches * batch_size, remainder);
-        //        R = NN.forward(A, B);
-        //        NN.backward();
-        //        NN.update(remainder);
-        //    }
-        //}
     }
 }
